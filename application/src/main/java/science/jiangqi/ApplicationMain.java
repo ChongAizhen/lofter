8e01b5917be5ee134c4cd2764f007f7ef1888ff7
chongaizhen <18363999928@163.com>
2018-04-17 22:34:54 +0800

    新增MapperScan

--- application/src/main/java/science/jiangqi/ApplicationMain.java
+++ application/src/main/java/science/jiangqi/ApplicationMain.java
@@ -1,11 +1,13 @@
 package science.jiangqi;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@MapperScan("science.jiangqi.dao")
 @EnableSwagger2
 public class ApplicationMain {
     public static void main(String[] args) {
